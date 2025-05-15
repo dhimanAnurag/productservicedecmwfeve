e269a7633b514a57edad82fb6cb4c1dac21a1f5e
dhimanAnurag <agoodinternetuser@gmail.com>
2025-05-15 06:38:46 +0530

    understand and implement the example of the HPA queries

--- src/main/java/com/scaler/productservicedecmwfeve/repositories/ProductRepository.java
+++ src/main/java/com/scaler/productservicedecmwfeve/repositories/ProductRepository.java
@@ -2,7 +2,9 @@ package com.scaler.productservicedecmwfeve.repositories;
 
 import com.scaler.productservicedecmwfeve.models.Category;
 import com.scaler.productservicedecmwfeve.models.Product;
+import com.scaler.productservicedecmwfeve.repositories.projections.ProductWithIdAndTitle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.lang.NonNullApi;
 import org.springframework.stereotype.Repository;
 
@@ -13,11 +15,11 @@ import java.util.Optional;
 public interface ProductRepository
     extends JpaRepository<Product, Long> {
 
-
-
     Optional<Product> findById(Long id);
     // this will return a null ifno product matches the id
 
+    @Query("Select p.id as id , p.title as title, p.description as description from Product p where p.id = 2")
+     List<ProductWithIdAndTitle> someHpaExample();
     Product save(Product product);
 }
 
--- src/test/java/com/scaler/productservicedecmwfeve/ProductservicedecmwfeveApplicationTests.java
+++ src/test/java/com/scaler/productservicedecmwfeve/ProductservicedecmwfeveApplicationTests.java
@@ -1,13 +1,33 @@
 package com.scaler.productservicedecmwfeve;
 
+import com.scaler.productservicedecmwfeve.models.Product;
+import com.scaler.productservicedecmwfeve.repositories.ProductRepository;
+import com.scaler.productservicedecmwfeve.repositories.projections.ProductWithIdAndTitle;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+
+import java.util.List;
 
 @SpringBootTest
 class ProductservicedecmwfeveApplicationTests {
-
+    @Autowired
+    private ProductRepository productRepository;
     @Test
     void contextLoads() {
     }
 
+    @Test
+    @Transactional
+    @Commit
+    void testQueries() {
+     List<ProductWithIdAndTitle> products = productRepository.someHpaExample();
+      for(ProductWithIdAndTitle product : products) {
+          System.out.println(product.getId());
+          System.out.println(product.getTitle());
+          System.out.println(product.getDescription());
+      }
+    }
 }
