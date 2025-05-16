64b72bd3363b203a4f3b37311537fcf706886e39
dhimanAnurag <agoodinternetuser@gmail.com>
2025-05-16 11:25:18 +0530

    small latest changes

--- src/main/java/com/scaler/productservicedecmwfeve/models/Category.java
+++ src/main/java/com/scaler/productservicedecmwfeve/models/Category.java
@@ -2,13 +2,18 @@ package com.scaler.productservicedecmwfeve.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
 public class Category extends BaseModel {
+    @OneToMany(mappedBy = "category") // being already mapped the attribute called category
+    private List<Product> products;
     private String name;
 
 //    public String getName() {
