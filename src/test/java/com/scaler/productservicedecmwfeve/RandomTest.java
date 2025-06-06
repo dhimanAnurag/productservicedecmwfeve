1c9797c09046887facd2fd4faead23112be374e7
dhimanAnurag <agoodinternetuser@gmail.com>
2025-06-07 00:08:03 +0530

    implementing the test cases

---
+++ src/test/java/com/scaler/productservicedecmwfeve/RandomTest.java
@@ -0,0 +1,31 @@
+package com.scaler.productservicedecmwfeve;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RandomTest {
+    @Test
+    void testOnePlusOneIsTwo() {
+        int i = 1 + 1; //act
+        assert i == 2;
+        assertEquals(2, i);
+    }
+
+    @Test
+    void testTwoTwoZaFour() {
+
+    }
+
+
+}
+
+
+// a test case is nothing but a method
+// test case method is doing: AAA
+// Arrange
+// Act
+// Assert
+// A test case fails in any assertion within a test case fails
---
+++ src/test/java/com/scaler/productservicedecmwfeve/controllers/ProductControllerTest.java
@@ -0,0 +1,7 @@
+package com.scaler.productservicedecmwfeve.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductControllerTest {
+
+}
